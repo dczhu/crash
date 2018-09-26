20074b95e456325e7d9b09f5761d9bf8af1c393a
Dengcheng Zhu <dzhu@wavecomp.com>
2018-09-26 11:37:10 -0700

    task: Fix "panic task not found"
    Searching panic task via foreach backtrace should look for __crash_kexec in
    addition to crash_kexec and .crash_kexec.

--- task.c
+++ task.c
@@ -7136,7 +7136,8 @@ panic_search(void)
 		}
 
 		if (strstr(buf, " crash_kexec at ") ||
-		    strstr(buf, " .crash_kexec at ")) {
+		    strstr(buf, " .crash_kexec at ") ||
+		    strstr(buf, " __crash_kexec at ")) {
 			found = TRUE;
 			break;	
 		}
